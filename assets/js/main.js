1813bbef4f312de5eb9fab778a3d78b1987e1fff
emeshitaafrin <emeshitaafrin@gmail.com>
2022-08-23 21:43:31 +0600

    added addEventListner Btn five to Eight

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,3 +104,111 @@ document.getElementById('selected-btn4').addEventListener('click',function(){
     }
 });
 // Button Four End
+
+// Button Five Start
+document.getElementById('selected-btn5').addEventListener('click',function(){
+    //length check
+    const selectedPlayerlength= document.querySelectorAll('#selected-player li');
+    if(selectedPlayerlength.length < 5){
+        // disabled btn
+        const btnDesabaled= document.getElementById('selected-btn5');
+        btnDesabaled.setAttribute('disabled',true);
+        btnDesabaled.style.backgroundColor='gray';
+        //player name select
+        const name = document.getElementById('selected-name5');
+        const elemet= name.innerText;
+    
+        //add 
+         const selectedPlayer= document.getElementById('selected-player');
+        const createElement= document.createElement('li');
+        createElement.innerText=elemet;
+
+    selectedPlayer.appendChild(createElement);
+
+    }else{
+    alert('Sorry!!You have crossed limit.');
+    }
+});
+// Button Five End
+
+// Button Six Start
+document.getElementById('selected-btn6').addEventListener('click',function(){
+    //length check
+    const selectedPlayerlength= document.querySelectorAll('#selected-player li');
+    if(selectedPlayerlength.length < 5){
+        // disabled btn
+        const btnDesabaled= document.getElementById('selected-btn6');
+        btnDesabaled.setAttribute('disabled',true);
+         btnDesabaled.style.backgroundColor='gray';
+        //player name select
+        const name = document.getElementById('selected-name6');
+        const elemet= name.innerText;
+    
+        //add 
+        const selectedPlayer= document.getElementById('selected-player');
+        const createElement= document.createElement('li');
+        createElement.innerText=elemet;
+
+    selectedPlayer.appendChild(createElement);
+    
+    }else{
+    alert('Sorry!!You have crossed limit.');
+    }
+});
+// Button Six End
+
+// Button Seven Start
+document.getElementById('selected-btn7').addEventListener('click',function(){
+   //length check
+   const selectedPlayerlength= document.querySelectorAll('#selected-player li');
+   if(selectedPlayerlength.length < 5){
+
+        // disabled btn
+        const btnDesabaled= document.getElementById('selected-btn7');
+        btnDesabaled.setAttribute('disabled',true);
+        btnDesabaled.style.backgroundColor='gray';
+
+       //player name 
+       const name = document.getElementById('selected-name7');
+       const elemet= name.innerText;
+   
+       //add 
+       const selectedPlayer= document.getElementById('selected-player');
+       const createElement= document.createElement('li');
+       createElement.innerText=elemet;
+
+   selectedPlayer.appendChild(createElement);
+ 
+   }else{
+   alert('Sorry!!You have crossed limit.');
+   }
+});
+// Button Seven End
+
+// Button Eight Start
+document.getElementById('selected-btn8').addEventListener('click',function(){
+   //length check
+   const selectedPlayerlength= document.querySelectorAll('#selected-player li');
+   if(selectedPlayerlength.length < 5){
+
+        // disabled btn
+        const btnDesabaled= document.getElementById('selected-btn8');
+        btnDesabaled.setAttribute('disabled',true);
+        btnDesabaled.style.backgroundColor='gray';
+        //player name 
+        const name = document.getElementById('selected-name8');
+        const elemet= name.innerText;
+   
+        //add 
+        const selectedPlayer= document.getElementById('selected-player');
+        const createElement= document.createElement('li');
+        createElement.innerText=elemet;
+
+   selectedPlayer.appendChild(createElement);
+
+   }else{
+   alert('Sorry!!You have crossed limit');
+   }
+});
+// Button Eight End
+
