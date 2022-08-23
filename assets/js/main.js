b7586a4dca3c5e4704e247aa142c0fe9c104551e
emeshitaafrin <emeshitaafrin@gmail.com>
2022-08-23 21:38:05 +0600

    added addEventListner Button two & three

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Button One Start
 document.getElementById('selected-btn1').addEventListener('click',function(){
     //length check
     const selectedPlayerlength= document.querySelectorAll('#selected-player li');
@@ -22,6 +23,9 @@ document.getElementById('selected-btn1').addEventListener('click',function(){
     }
 
 });
+// Button One End
+
+// Button Two Start
 document.getElementById('selected-btn2').addEventListener('click',function(){
     //length check
     const selectedPlayerlength= document.querySelectorAll('#selected-player li');
@@ -46,3 +50,57 @@ document.getElementById('selected-btn2').addEventListener('click',function(){
     }
 
 });
+// Button Two End
+
+// Button Three Start
+document.getElementById('selected-btn3').addEventListener('click',function(){
+
+    //length check
+    const selectedPlayerlength= document.querySelectorAll('#selected-player li');
+    if(selectedPlayerlength.length < 5){
+         // disabled btn
+        const btnDesabaled= document.getElementById('selected-btn3');
+        btnDesabaled.setAttribute('disabled',true);
+        btnDesabaled.style.backgroundColor='gray';
+        //player name select
+        const name = document.getElementById('selected-name3');
+        const elemet= name.innerText;
+
+        //add 
+        const selectedPlayer= document.getElementById('selected-player');
+        const createElement= document.createElement('li');
+        createElement.innerText=elemet;
+
+    selectedPlayer.appendChild(createElement);
+
+    }else{
+    alert('Sorry!!You have crossed limit.');
+    }
+
+});
+// Button Three End
+
+// Button Four Start
+document.getElementById('selected-btn4').addEventListener('click',function(){
+    //length check
+    const selectedPlayerlength= document.querySelectorAll('#selected-player li');
+    if(selectedPlayerlength.length < 5){
+        // disabled btn
+        const btnDesabaled= document.getElementById('selected-btn4');
+        btnDesabaled.setAttribute('disabled',true);
+        btnDesabaled.style.backgroundColor='gray';
+        //player name select
+        const name = document.getElementById('selected-name4');
+        const elemet= name.innerText;
+        //add  
+        const selectedPlayer= document.getElementById('selected-player');
+        const createElement= document.createElement('li');
+        createElement.innerText=elemet;
+
+    selectedPlayer.appendChild(createElement);
+ 
+    }else{
+    alert('Sorry!!You have crossed limit.');
+    }
+});
+// Button Four End
